71e2f617daa044a5a2f37e32344d02fb1c0c4128
Glen Chao <glenchao@outlook.com>
2014-05-05 22:12:25 -0700

    Facebook + Styling
    * Added some styling
    * Added facebook personal info query

--- js/home.js
+++ js/home.js
@@ -7,8 +7,11 @@
         ready: function (element, options) {
             // TODO: Initialize the page here.
             WinJS.Utilities.query("a").listen("click", linkClickEventHandler, false);
+            WinJS.Utilities.query("#facebookLoginBtn").listen("click", logIntoFacebook, false);
         }
     });
+
+    var facebookUrl = "https://graph.facebook.com";
 
     function linkClickEventHandler(eventInfo) {
         eventInfo.preventDefault();
@@ -16,4 +19,43 @@
         WinJS.Navigation.navigate(link.href);
     }
 
+    function logIntoFacebook() {
+        var secureWeb = Windows.Security.Authentication.Web;
+        var endUri = "ms-app://s-1-15-2-2675179045-997083443-3805322078-311227240-3561484885-1850611153-4201577455"; //secureWeb.WebAuthenticationBroker.getCurrentApplicationCallbackUri().rawUri;
+        var requestUri = new Windows.Foundation.Uri(
+                                "https://www.facebook.com/dialog/oauth?" +
+                                "client_id=857337164293100&" +
+                                "display=popup&" +
+                                "response_type=token&" + 
+                                "redirect_uri=" + endUri);
+        
+        secureWeb.WebAuthenticationBroker.authenticateAsync(secureWeb.WebAuthenticationOptions.none, requestUri)
+        .done(function (result) {
+            var responseData = result.responseData.replace("#", "?"); // hack the response data a bit...
+            var responseUri = new Windows.Foundation.Uri(responseData);
+            var access_token = responseUri.queryParsed[0].value;
+            var expires_in = responseUri.queryParsed[1].value;
+            getUserInfo(access_token);
+        }, 
+        function (err) {
+            console.log(err);
+        });
+    }
+
+    function getUserInfo(accessToken) {
+        WinJS.xhr({
+            type: "GET",
+            url: facebookUrl + "/me" + "?access_token=" + accessToken,
+            responseType: "json",
+        }).done(
+            function completed(result) {
+                if (result.status === 200) {
+                    var user = JSON.parse(result.response);
+                    console.log(result.response);
+                }
+            },
+            function error(result) {
+
+            });
+    }
 })();
